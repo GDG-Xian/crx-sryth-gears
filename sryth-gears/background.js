f70cfd36e3cc516dae01bfa14f83eef293dc87e8
greatghoul <greatghoul@gmail.com>
2012-11-21 01:42:14 +0800

    Click extension icon to access sryth official site.

--- sryth-gears/background.js
+++ sryth-gears/background.js
@@ -6,6 +6,13 @@ function set_option(name, value) {
     localStorage[name] = value;
 }
 
+chrome.tabs.getSelected(null, function(tab) {
+    // When the browser action is clicked, set the popup for |tab|.
+    chrome.browserAction.onClicked.addListener(function(tab) {
+        chrome.tabs.create({ url: 'http://www.sryth.com/', active: true });
+    });
+});
+
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.type === 'GET_FONT_SIZE') {
